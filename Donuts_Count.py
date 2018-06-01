396c91b6c0fbd5d5bc1b1240a95e994c88e65836
Nishant Sharma <39654046+Nishant410@users.noreply.github.com>
2018-06-01 13:46:57 +0530

    Add files via upload

---
+++ Donuts_Count.py
@@ -0,0 +1,13 @@
+#This basic script takes the input from user and prints the number of
+# donuts eaten.
+def donuts(count):
+   if count <=10:
+     print ('total number of donuts eaten are', count)
+   else:
+     print ('many donuts were eaten')
+	
+count= int(input("enter number:"))
+donuts(count)
+def main():
+  if __name__ == '__main__':
+   main()
